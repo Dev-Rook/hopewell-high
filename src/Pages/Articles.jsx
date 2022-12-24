0ffedcb4331cbd6c642745df2600b08bdffc57ff
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-12-23 23:34:23 -0500

    Blog Page Shell

--- src/Pages/Articles.jsx
+++ src/Pages/Articles.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Styles from "../Styles/Page-Styles/Articles.module.scss";
+
+import Apply from "../Page-Sections/Apply";
+import Blog from "../Page-Sections/Blog";
+
+import BlogData from "../Assets/Data/Blog.json";
 
 const Articles = () => {
+  const [data, setData] = useState(BlogData);
   return (
-    <div>Articles</div>
-  )
-}
+    <div className={Styles.Page}>
+      <div className={Styles.PageHead}>
+        <div className={Styles.Diffuser}></div>
+        <img src={``} alt="" className={Styles.Heade_Image} />
+      </div>
 
-export default Articles
+      <div className={Styles.Section}>
+        <div className={Styles.Content_Container}>
+          {data &&
+            data.map((value) => {
+              return (
+                <div className={Styles.Blog_Card} key={value.id}>
+                  <div className={Styles.Image_Container}>
+                    <p className={Styles.Category}>{value.Category}</p>
+                    <img src={value.Image} alt="" className={Styles.Image} />
+                  </div>
+                  <div className={Styles.Information_Box}>
+                    <p className={Styles.Tite}>{value.Title}</p>
+                    <p className={Styles.Date}>{value.Date}</p>
+                    <p className={Styles.Description}>{value.Description}</p>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+
+      {/* <Blog /> */}
+      <Apply />
+    </div>
+  );
+};
+
+export default Articles;
