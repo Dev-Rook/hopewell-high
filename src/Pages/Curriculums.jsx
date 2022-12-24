e4b43590f02a97b4ac86db636fe7718b60e0f3b9
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-12-23 23:45:20 -0500

    Curriculums Page Shell

--- src/Pages/Curriculums.jsx
+++ src/Pages/Curriculums.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Styles from "../Styles/Page-Styles/Curriculums.module.scss";
+
+import Apply from "../Page-Sections/Apply";
+import ServiceData from "../Assets/Data/Programs.json";
 
 const Curricumlums = () => {
+  const [data, setData] = useState(ServiceData);
   return (
-    <div>Curricumlums</div>
-  )
-}
+    <div className={Styles.Page}>
+      <div className={Styles.PageHead}>
+        <div className={Styles.Diffuser}></div>
+        <img src={``} alt="" className={Styles.Heade_Image} />
+      </div>
 
-export default Curricumlums
+      <div className={Styles.Section}>
+        <div className={Styles.Content_Container}>
+          {data &&
+            data.map((value) => {
+              return (
+                <div className={Styles.Card} key={value.id}>
+                  <img src={value.Icon} alt="" className={Styles.Icon} />
+                  <p className={Styles.Title}>{value.Title}</p>
+                  <p className={Styles.Description}>{value.Description}</p>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+
+      <Apply />
+    </div>
+  );
+};
+
+export default Curricumlums;
